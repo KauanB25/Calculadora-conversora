0e836ede080f1d322dbc8fd0c17bafd3ed5d6096
Kauan Bezerra <kaauan748@gmail.com>
2022-12-22 19:10:46 -0300

    botão C funcionando

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,7 @@
 function inserir(num){
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;}
+
+function limpar(num){
+    document.getElementById('resultado').innerHTML=""
+}
