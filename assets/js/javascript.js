c664906ecdaf7f94514fc5e328f9b89a456d616f
Kauan Bezerra <kaauan748@gmail.com>
2022-12-26 18:12:19 -0300

    botão de multiplica e virgula pendentes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,3 +5,32 @@ function inserir(num){
 function limpar(num){
     document.getElementById('resultado').innerHTML=""
 }
+function multiplicar(num){
+    const resultado_anterior= document.getElementById('resultado').textContent
+    inserir('x')
+    console.log(resultado_anterior)
+    let visor= resultado_anterior.innerHTML
+    let real=resultado_anterior + '*'
+    console.log(real)
+
+}
+function reset()
+        {
+            var resultado = document.getElementById('resultado').innerHTML;
+            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+        }
+function calcular()
+        {
+            var resultado = document.getElementById('resultado').innerHTML;
+            if(resultado)
+            {
+                document.getElementById('resultado').innerHTML = eval(resultado);
+            }
+            else
+            {
+                document.getElementById('resultado').innerHTML = "Nada..."
+            }
+        }
+
+var test = document.getElementById('botao_reset');
+console.log(test.outerText)
