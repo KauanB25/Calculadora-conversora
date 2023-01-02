08c93a0396e264c45341cd0e1b7c69ccb7701df3
Kauan Bezerra <kaauan748@gmail.com>
2023-01-02 10:10:58 -0300

    Exclusão de linhas desnecessárias.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,7 +2,6 @@ let operador=0
 let real=''
 
 function inserir(num,opera=false, multi=false){
-    //console.log(operador)
     if(opera==true){
         operador+=1
         if(operador>=2){
@@ -10,7 +9,6 @@ function inserir(num,opera=false, multi=false){
         }
         opera=false
     }
-    //console.log(operador)
     
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
@@ -22,15 +20,7 @@ function limpar(num){
     document.getElementById('resultado').innerHTML=""
     operador=0
 }
-/*function multiplicar(num){
-    const resultado_anterior= document.getElementById('resultado').textContent
-    inserir('x')
-    console.log(resultado_anterior)
-    let visor= resultado_anterior.innerHTML
-    let real=resultado_anterior + '*'
-    console.log(real)
 
-}*/
 function reset()
         {
             var resultado = document.getElementById('resultado').innerHTML;
@@ -54,5 +44,3 @@ function calcular()
             operador-=1
         }
 
-var test = document.getElementById('botao_reset');
-console.log(test.outerText)
